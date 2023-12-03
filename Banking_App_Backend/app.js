468541805745d767d75a3432c6721107151481e9
aslamxdunk2 <149433535+aslamxdunk2@users.noreply.github.com>
2023-12-03 01:29:08 -0600

    2nd Implementation of Backend Overhaul

--- Banking_App_Backend/app.js
+++ Banking_App_Backend/app.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://sayeeduddin:hello@test.pkxcqvn.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -28,14 +27,35 @@ async function run() {
     await client.close();
   }
 }
+
 run().catch(console.dir);
 // Define schemas
+/*
 const AccountSchema = new mongoose.Schema({
   accountNumber: { type: String, unique: true },
   joinDate: { type: Date, default: Date.now },
   rewardTypeLastRedeemed: String,
   xpAmount: { type: Number, default: 0 },
+  milesAmount: { type: Number, default: 0 },
+  walmartAmount: { type: Number, default: 0 },
+  amazonAmount: { type: Number, default: 0 },
   lastRedeemTime: { type: Date, default: null },
+});*/
+
+const AccountSchema = new mongoose.Schema({
+  accountNumber: { type: String, unique: true },
+  name: String,
+  password: String,
+  joinDate: { type: Date, default: Date.now },
+  rewardTypeLastRedeemed: String,
+  xpAmount: { type: Number, default: 0 },
+  level: { type: Number, default: 1 },
+  checkingAmount: { type: Number, default: Math.floor(Math.random() * 10000) + 1000 },
+  savingsAmount: { type: Number, default: Math.floor(Math.random() * 50000) + 5000 },
+  miles: { type: Number, default: 0 },
+  walmartCredits: { type: Number, default: 0 },
+  amazonCredits: { type: Number, default: 0 },
+  targetCredits: { type: Number, default: 0 },
 });
 
 // Create mongoose model
@@ -47,6 +67,7 @@ app.use(bodyParser.json());
 // Define routes
 
 // Create a new account
+/*
 app.post('/api/account', async (req, res) => {
   try {
     const newAccount = new Account(req.body);
@@ -56,7 +77,153 @@ app.post('/api/account', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+*/
 
+// Create an Account with random data
+app.post('/api/account/createRandomAccount', async (req, res) => {
+  try {
+    const { name, password } = req.body;
+
+    const newAccount = new Account({
+      accountNumber: Math.random().toString(36).substring(7),
+      name,
+      password,
+    });
+
+    const savedAccount = await newAccount.save();
+    res.json(savedAccount);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/api/account/changeXP', async (req, res) => {
+  try {
+    const { accountNumber, password, xpAmount } = req.body;
+    const account = await Account.findOneAndUpdate(
+      { accountNumber, password },
+      { xpAmount },
+      { new: true }
+    );
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found or password incorrect' });
+    }
+
+    res.json({ xpAmount: account.xpAmount });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+app.post('/api/account/increaseLevel', async (req, res) => {
+  try {
+    const { accountNumber, password } = req.body;
+    const account = await Account.findOneAndUpdate(
+      { accountNumber, password },
+      { $inc: { level: 1 } },
+      { new: true }
+    );
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found or password incorrect' });
+    }
+
+    res.json({ level: account.level });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/api/account/increaseCapitalOneMiles', async (req, res) => {
+  try {
+    const { accountNumber, password, miles } = req.body;
+    const account = await Account.findOneAndUpdate(
+      { accountNumber, password },
+      { $inc: { miles } },
+      { new: true }
+    );
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found or password incorrect' });
+    }
+
+    res.json({ miles: account.miles });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/api/account/increaseWalmartCredits', async (req, res) => {
+  try {
+    const { accountNumber, password, walmartCredits } = req.body;
+    const account = await Account.findOneAndUpdate(
+      { accountNumber, password },
+      { $inc: { walmartCredits } },
+      { new: true }
+    );
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found or password incorrect' });
+    }
+
+    res.json({ walmartCredits: account.walmartCredits });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/api/account/increaseAmazonCredits', async (req, res) => {
+  try {
+    const { accountNumber, password, amazonCredits } = req.body;
+    const account = await Account.findOneAndUpdate(
+      { accountNumber, password },
+      { $inc: { amazonCredits } },
+      { new: true }
+    );
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found or password incorrect' });
+    }
+
+    res.json({ amazonCredits: account.amazonCredits });
+  } catch (error) {
+    res.status(500).json({ error: error.message});
+  }
+});
+
+// Increase Target Credits
+app.post('/api/account/increaseTargetCredits', async (req, res) => {
+  try {
+    const { accountNumber, password, targetCredits } = req.body;
+    const account = await Account.findOneAndUpdate(
+      { accountNumber, password },
+      { $inc: { targetCredits } },
+      { new: true }
+    );
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found or password incorrect' });
+    }
+
+    res.json({ targetCredits: account.targetCredits });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+// VERSION 1 Implementation 
+/*
 // Get account details by account number
 app.get('/api/account/:accountNumber', async (req, res) => {
   try {
@@ -200,10 +367,93 @@ app.get('/api/account/:accountNumber/lastRedeemTime', async (req, res) => {
   }
 });
 
+// Increase XP amount by 50 for a specific account number
+app.post('/api/account/:accountNumber/increaseXP', async (req, res) => {
+  try {
+    const account = await Account.findOne({ accountNumber: req.params.accountNumber });
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found' });
+    }
+
+    // Increase XP by 50
+    const updatedXP = account.xpAmount + 50;
+
+    // Update account with the increased XP amount
+    await Account.updateOne({ accountNumber: req.params.accountNumber }, { xpAmount: updatedXP });
+
+    res.json({ xpAmount: updatedXP });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Increase XP amount by 50 for a specific account number
+app.post('/api/account/:accountNumber/increaseMiles', async (req, res) => {
+  try {
+    const account = await Account.findOne({ accountNumber: req.params.accountNumber });
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found' });
+    }
+
+    // Increase XP by 50
+    const updatedMiles = account.milesAmount + 50;
+
+    // Update account with the increased XP amount
+    await Account.updateOne({ accountNumber: req.params.accountNumber }, { milesAmount: updatedXP });
+
+    res.json({ xpAmount: updatedMiles });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Increase XP amount by 50 for a specific account number
+app.post('/api/account/:accountNumber/increaseXP', async (req, res) => {
+  try {
+    const account = await Account.findOne({ accountNumber: req.params.accountNumber });
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found' });
+    }
+
+    // Increase XP by 50
+    const updatedXP = account.xpAmount + 50;
+
+    // Update account with the increased XP amount
+    await Account.updateOne({ accountNumber: req.params.accountNumber }, { xpAmount: updatedXP });
+
+    res.json({ xpAmount: updatedXP });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+// Increase XP amount by 50 for a specific account number
+app.post('/api/account/:accountNumber/increaseXP', async (req, res) => {
+  try {
+    const account = await Account.findOne({ accountNumber: req.params.accountNumber });
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found' });
+    }
+
+    // Increase XP by 50
+    const updatedXP = account.xpAmount + 50;
+
+    // Update account with the increased XP amount
+    await Account.updateOne({ accountNumber: req.params.accountNumber }, { xpAmount: updatedXP });
+
+    res.json({ xpAmount: updatedXP });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 
 // ... (other routes)
-
+*/
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
