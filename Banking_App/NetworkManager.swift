bf9c3f797de87d985aace48f4e770e9a0cdcee11
Patron <Patron@Patrons-MacBook-Air.local>
2023-12-08 17:52:24 -0600

    Added database getters

--- Banking_App/NetworkManager.swift
+++ Banking_App/NetworkManager.swift
@@ -81,6 +81,185 @@ class NetworkManager {
             }.resume()
         }
     
+    // GET LEVEL
+    func getLevel(accountNumber: String, completion: @escaping (Result<Int, Error>) -> Void) {
+        // Construct the URL
+        guard let url = URL(string: "https://pacific-lake-00352-372639b432c2.herokuapp.com/api/account/\(accountNumber)/level") else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+
+        // Create an HTTP GET request
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        // Create a URLSession task
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                DispatchQueue.main.async { completion(.failure(error)) }
+                return
+            }
+
+            guard let data = data else {
+                DispatchQueue.main.async { completion(.failure(NetworkError.invalidResponse)) }
+                return
+            }
+
+            do {
+                let level = try JSONDecoder().decode(Int.self, from: data)
+                DispatchQueue.main.async { completion(.success(level)) }
+            } catch {
+                DispatchQueue.main.async { completion(.failure(error)) }
+            }
+        }
+
+        // Start the URLSession task
+        task.resume()
+    }
+    
+    // GET CAPITAL ONE MILES
+    func getCapitalOneMiles(accountNumber: String, completion: @escaping (Result<Int, Error>) -> Void) {
+        // Construct the URL
+        guard let url = URL(string: "https://pacific-lake-00352-372639b432c2.herokuapp.com/api/account/\(accountNumber)/capitalOneMiles") else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+
+        // Create an HTTP GET request
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        // Create a URLSession task
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                DispatchQueue.main.async { completion(.failure(error)) }
+                return
+            }
+
+            guard let data = data else {
+                DispatchQueue.main.async { completion(.failure(NetworkError.invalidResponse)) }
+                return
+            }
+
+            do {
+                let capitalOneMiles = try JSONDecoder().decode(Int.self, from: data)
+                DispatchQueue.main.async { completion(.success(capitalOneMiles)) }
+            } catch {
+                DispatchQueue.main.async { completion(.failure(error)) }
+            }
+        }
+
+        // Start the URLSession task
+        task.resume()
+    }
+    
+    // GET WALMART CREDITS
+    func getWalmartCredits(accountNumber: String, completion: @escaping (Result<Int, Error>) -> Void) {
+        // Construct the URL
+        guard let url = URL(string: "https://pacific-lake-00352-372639b432c2.herokuapp.com/api/account/\(accountNumber)/walmartCredits") else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+
+        // Create an HTTP GET request
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        // Create a URLSession task
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                DispatchQueue.main.async { completion(.failure(error)) }
+                return
+            }
+
+            guard let data = data else {
+                DispatchQueue.main.async { completion(.failure(NetworkError.invalidResponse)) }
+                return
+            }
+
+            do {
+                let walmartAmount = try JSONDecoder().decode(Int.self, from: data)
+                DispatchQueue.main.async { completion(.success(walmartAmount)) }
+            } catch {
+                DispatchQueue.main.async { completion(.failure(error)) }
+            }
+        }
+
+        // Start the URLSession task
+        task.resume()
+    }
+    
+    // GET AMAZON CREDITS
+    func getAmazonCredits(accountNumber: String, completion: @escaping (Result<Int, Error>) -> Void) {
+        // Construct the URL
+        guard let url = URL(string: "https://pacific-lake-00352-372639b432c2.herokuapp.com/api/account/\(accountNumber)/amazonCredits") else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+
+        // Create an HTTP GET request
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        // Create a URLSession task
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                DispatchQueue.main.async { completion(.failure(error)) }
+                return
+            }
+
+            guard let data = data else {
+                DispatchQueue.main.async { completion(.failure(NetworkError.invalidResponse)) }
+                return
+            }
+
+            do {
+                let amazonCredits = try JSONDecoder().decode(Int.self, from: data)
+                DispatchQueue.main.async { completion(.success(amazonCredits)) }
+            } catch {
+                DispatchQueue.main.async { completion(.failure(error)) }
+            }
+        }
+
+        // Start the URLSession task
+        task.resume()
+    }
+    
+    // GET TARGET CREDITS
+    func getTargetCredits(accountNumber: String, completion: @escaping (Result<Int, Error>) -> Void) {
+        // Construct the URL
+        guard let url = URL(string: "https://pacific-lake-00352-372639b432c2.herokuapp.com/api/account/\(accountNumber)/targetCredits") else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+
+        // Create an HTTP GET request
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        // Create a URLSession task
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                DispatchQueue.main.async { completion(.failure(error)) }
+                return
+            }
+
+            guard let data = data else {
+                DispatchQueue.main.async { completion(.failure(NetworkError.invalidResponse)) }
+                return
+            }
+
+            do {
+                let targetCredits = try JSONDecoder().decode(Int.self, from: data)
+                DispatchQueue.main.async { completion(.success(targetCredits)) }
+            } catch {
+                DispatchQueue.main.async { completion(.failure(error)) }
+            }
+        }
+
+        // Start the URLSession task
+        task.resume()
+    }
 }
 
 enum NetworkError: Error {
